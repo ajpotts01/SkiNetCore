668f834de562cf636ef6082aa27143ddce5a7029
Alexander Potts <alex.potts@edg.com.au>
2022-12-29 16:32:18 +1100

    End of Angular client basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const API_ENDPOINT_ADDRESS: string = 'https://localhost:5001/api/products?pageSize=50';
+
+    // Course material used deprecated subscribe - now using one Observable object.
+    this.http.get(API_ENDPOINT_ADDRESS).subscribe({
+      next: (response: IPagination) => { this.products = response.data; console.log('Success') },
+      error: (error) => { console.log('Error'); console.log(error) }
+    });
+
+    // this.http.get(API_ENDPOINT_ADDRESS).subscribe((response: IPagination) => {
+    //   this.products = response.data;
+    // }, error => {
+    //   console.log(error);
+    // });
+  }  
 }
