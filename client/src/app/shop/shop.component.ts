9e7d3a0ba8119f7de5dcac109be0332da8288e36
Alexander Potts <alex.potts@edg.com.au>
2022-12-29 23:06:10 +1100

    Services, filtering, etc.

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
+import { IProductType } from '../shared/models/product-type';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,14 +11,53 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  brands: IBrand[];
+  productTypes: IProductType[];
+  selectedBrandId: number;
+  selectedTypeId: number;
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
-      next: (response) => { this.products = response.data; console.log('Shop load: success')},
-      error: (error) => { console.log('Shop load: error'); console.log(error) }
+    this.getProducts();
+    this.getBrands();
+    this.getProductTypes();
+  }
+
+  // Note: getProducts uses response.data while brands/types don't
+  // this is because products is paginated data e.g. IPagination with encapsulated data field
+  // The others are just plain arrays
+  getProducts() {
+    this.shopService.getProducts(this.selectedBrandId, this.selectedTypeId).subscribe({
+      next: (response) => { this.products = response.data; console.log('Shop load - products: success') },
+      error: (error) => { console.log('Shop load - products: error'); console.log(error) }
     });
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe({
+      next: (response) => { this.brands = [{id: 0, name: 'All'}, ...response]; console.log('Shop load - brands: success') },
+      error: (error) => { console.log('Shop load - brands: error'); console.log(error) }
+    });
+  }
+
+  getProductTypes() {
+    this.shopService.getProductTypes().subscribe({
+      next: (response) => { this.productTypes = [{id: 0, name: 'All'}, ...response]; console.log('Shop load - types: success') },
+      error: (error) => { console.log('Shop load - types: error'); console.log(error) }
+    })
+  }
+
+  onBrandSelected(brandId: number) {
+    this.selectedBrandId = brandId;
+
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.selectedTypeId = typeId;
+
+    this.getProducts();
   }
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,16 +1,57 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IProductType } from '../shared/models/product-type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
-  baseUrl = 'https://localhost:5001/api/';
+
+  private readonly API_URL_BASE: string = 'https://localhost:5001/api';
+  private readonly API_ENDPOINT_PRODUCTS: string = 'products';
+  private readonly API_ENDPOINT_PRODUCTS_BRANDS: string = 'brands';
+  private readonly API_ENDPOINT_PRODUCTS_TYPES: string = 'types';
+  private readonly API_QUERY_PAGESIZE_50: string = 'pageSize=50'; // placeholder until params are added
+
+  private readonly API_QUERY_DEFAULT_PAGESIZE: number = 50;
+  private readonly API_QUERY_KEY_PAGESIZE: string = 'pageSize';
+  private readonly API_QUERY_KEY_BRAND_ID: string = 'brandId';
+  private readonly API_QUERY_KEY_TYPE_ID: string = 'typeId';
 
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  getProducts(brandId?: number, typeId?: number) {
+    let params = new HttpParams();
+
+    params = params.append(this.API_QUERY_KEY_PAGESIZE, this.API_QUERY_DEFAULT_PAGESIZE.toString());
+
+    if (brandId) {
+      params = params.append(this.API_QUERY_KEY_BRAND_ID, brandId.toString());
+    }
+
+    if (typeId) {
+      params = params.append(this.API_QUERY_KEY_TYPE_ID, typeId.toString());
+    }
+
+    // e.g. api/products?pageSize=50
+    return this.http.get<IPagination>(`${this.API_URL_BASE}/${this.API_ENDPOINT_PRODUCTS}`, { observe: 'response', params })
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
+  }
+
+  getProductTypes() {
+    // e.g. api/products/types
+    return this.http.get<IProductType[]>(`${this.API_URL_BASE}/${this.API_ENDPOINT_PRODUCTS}/${this.API_ENDPOINT_PRODUCTS_TYPES}`)
+  }
+
+  getBrands() {
+    // e.g. api/products/brands
+    return this.http.get<IBrand[]>(`${this.API_URL_BASE}/${this.API_ENDPOINT_PRODUCTS}/${this.API_ENDPOINT_PRODUCTS_BRANDS}`);
   }
 }
