a3cc1abafea6218285c27296b29386a5d617fa47
Alexander Potts <alex.potts@edg.com.au>
2023-01-02 16:56:02 +1100

    Initial work

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,9 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, 
+              private activatedRoute: ActivatedRoute, 
+              private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -19,7 +22,11 @@ export class ProductDetailsComponent implements OnInit {
 
   loadProduct() {
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
-      next: product => { console.log('Shop - product details - load succeeded'); this.product = product; },
+      next: product => { 
+        console.log('Shop - product details - load succeeded'); 
+        this.product = product; 
+        this.bcService.set('@productDetails', product.name);
+      },
       error: error => { console.log('Shop - product details - load product failed'); console.log(error); }
     });
   }
