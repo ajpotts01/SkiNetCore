b947d74c94dcb63ab1a76850a227896fd3158a7d
Alexander Potts <alex.potts@edg.com.au>
2023-04-22 23:10:38 +1000

    Unscrew package.json issues and fix up toastr

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL_BASE: 'https://localhost:5001/api',
+  API_ENDPOINT_PRODUCTS: 'products',
+  API_ENDPOINT_PRODUCTS_BRANDS: 'brands',
+  API_ENDPOINT_PRODUCTS_TYPES: 'types',
+  API_QUERY_PAGESIZE_50: 'pageSize=50', // placeholder until params are added
+
+  API_QUERY_DEFAULT_PAGESIZE: 50,
+  API_QUERY_KEY_PAGENUMBER: 'pageIndex',
+  API_QUERY_KEY_PAGESIZE: 'pageSize',
+  API_QUERY_KEY_BRAND_ID: 'brandId',
+  API_QUERY_KEY_TYPE_ID: 'typeId',
+  API_QUERY_KEY_SEARCH: 'search',
+  API_QUERY_KEY_SORT: 'sort'
 };
